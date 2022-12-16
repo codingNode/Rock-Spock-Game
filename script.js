a4c68fcddb0bba2a6bab5128360e59d75dc36021
Hasnat Gulbaz <hasnat.uetian145@gmail.com>
2022-12-16 18:15:57 +0500

    UI and computerChoice

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const playerChoice = document.getElementById('playerChoice');
 const playerScore  = document.getElementById('playerScore');
 const playerSelection = document.querySelectorAll('.player-container i');
+const computerSelections = document.querySelectorAll('.computer-container i');
+const computerChoiceText = document.getElementById('computerChoice');
+
 
 // console.log(playerSelection[0].title);
 // playerSelection[0].style.color = 'dodgerblue'
@@ -13,20 +16,52 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+
+let computerSelectedItem ='';
+
+function computerChoice()
+{
+  const options=['Rock','Paper','Scissors','Lizard','Spock'];
+
+  let randomNum = Math.floor(Math.random()*5) ;
+  computerSelectedItem = options[randomNum];
+
+  computerSelections.forEach((item,i)=>{
+  
+    computerSelections[i].classList.remove('computer-selected');
+
+    if(item.title === computerSelectedItem)
+    {
+      
+      computerSelections[i].classList.add('computer-selected');
+      computerChoiceText.textContent = ` ---${computerSelectedItem}`;
+      computerChoiceText.style.color = 'rgb(255, 30, 49)';
+    }
+  })
+
+}
+
+function checkResult()
+{
+  computerChoice()
+}
+
 function select(choice)
 {
   
-  playerSelection.forEach((item,i)=>{
+    playerSelection.forEach((item,i)=>{
   
-    playerSelection[i].classList.remove('selected');
+    playerSelection[i].classList.remove('player-selected');
 
     if(item.title === choice)
     {
       
       playerSelection[i].classList.add('player-selected');
       playerChoice.textContent = ` ---${choice}`;
-      playerChoice.style.color = 'dodgerblue'
+      playerChoice.style.color = 'dodgerblue';
     }
   })
+
+  checkResult()
 }
 
